13dc235d34dc0764d43d2137fc4b6d965dc28de2
Miifff <miftakhhisyam@gmail.com>
2026-04-29 14:27:37 +0700

    merge

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,10 +3,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public PlayerData playerData;
     public float currentHP;
+=======
+<<<<<<< Updated upstream
+    public float currentHP = 100;
+    public float speed = 5f;
+=======
+    public PlayerData playerData;
+
+    public float currentHP;
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     private PlayerInput playerInput;
     private Vector2 moveInput;
+    private float attackInput;
 
     void Start()
     {
@@ -24,11 +36,28 @@ public class PlayerController : MonoBehaviour
         if (playerInput == null) return;
         
         moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+
+        attackInput = playerInput.actions["Attack"].ReadValue<float>();
+
         float h = moveInput.x;
         float v = moveInput.y;
 
+<<<<<<< Updated upstream
         float currentSpeed = playerData != null ? playerData.moveSpeed : 5f;
         transform.Translate(new Vector3(h, v, 0) * currentSpeed * Time.deltaTime);
+=======
+<<<<<<< Updated upstream
+        transform.Translate(new Vector3(h, v, 0) * speed * Time.deltaTime);
+=======
+        float currentSpeed = playerData != null ? playerData.moveSpeed : 5f;
+        transform.Translate(new Vector3(h, v, 0) * currentSpeed * Time.deltaTime);
+
+        if (attackInput > 0) 
+        {
+            Shoot();
+        }
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     }
 
     void OnCollisionStay2D(Collision2D collision)
@@ -50,5 +79,10 @@ public class PlayerController : MonoBehaviour
         {
             GameManager.Instance.GameOver();
         }
+    }
+
+    void Shoot()
+    {
+        Debug.Log("Player shoots!");
     }
 }
